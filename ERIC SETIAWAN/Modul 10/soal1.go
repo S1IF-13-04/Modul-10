7ef6678c300f68e236e589398f19235a4cb91c98
Eric Setiawan <kimetsugay@gmail.com>
2025-11-23 23:25:42 +0700

    Add files via upload

---
+++ ERIC SETIAWAN/Modul 10/soal1.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var berat int
+	fmt.Print("Berat parsel (gram): ")
+	fmt.Scan(&berat)
+
+	kg := berat / 1000
+	sisa := berat % 1000
+
+	biayaKg := kg * 10000
+	biayaSisa := 0
+
+	if berat > 10000 {
+		biayaSisa = 0
+	} else {
+		if sisa < 500 {
+			biayaSisa = sisa * 5
+		} else {
+			biayaSisa = sisa * 15
+		}
+	}
+
+	total := biayaKg + biayaSisa
+
+	fmt.Printf("Detail berat: %d kg + %d gr\n", kg, sisa)
+	fmt.Printf("Detail biaya: Rp. %d + Rp. %d\n", biayaKg, biayaSisa)
+	fmt.Printf("Total biaya: Rp. %d\n", total)
+}
---
+++ ERIC SETIAWAN/Modul 10/soal3.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b int
+
+	fmt.Print("Bilangan: ")
+	fmt.Scan(&b)
+
+	fmt.Print("Faktor : ")
+
+	jumlahFaktor := 0
+
+	for i := 1; i <= b; i++ {
+		if b%i == 0 {
+			fmt.Print(" ", i)
+			jumlahFaktor++
+		}
+	}
+
+	fmt.Println()
+
+	isPrima := false
+	if jumlahFaktor == 2 {
+		isPrima = true
+	}
+
+	fmt.Printf("Prima  : %v\n", isPrima)
+}
